1b72fee0ed971f105a1b281b9f2ef14ba68908ab
Collins Lagat <collinslagat1901@gmail.com>
2025-09-16 12:44:41 +0000

    reduces the space between the icon and tehe text (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-1">
       <Image 
         src="https://nowqwttrqtklrxgjgxid.supabase.co/storage/v1/object/public/folder/photos/The%20Entity%20Icon.png" 
         alt="Thee Entity Icon"
