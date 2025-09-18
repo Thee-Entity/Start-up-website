3eebb4d28d9170986430701371dcaa3b774436a3
Collins Lagat <collinslagat1901@gmail.com>
2025-09-18 18:52:08 +0000

    sprry for the disturbance change use this instead https://nowqwttrqtklrx

--- src/components/sections/team-section.tsx
+++ src/components/sections/team-section.tsx
@@ -27,7 +27,7 @@ const founders = [
     name: "Zeituna Kurr",
     role: "Marketing & Social Media Manager",
     philosophy: "Telling great stories about great technology.",
-    imageUrl: "https://nowqwttrqtklrxgjgxid.supabase.co/storage/v1/object/public/folder/photos/Zeituna.jpg",
+    imageUrl: "https://nowqwttrqtklrxgjgxid.supabase.co/storage/v1/object/public/folder/photos/Zeituna%20Kurr.jpg",
     aiHint: "female marketer"
   }
 ];
